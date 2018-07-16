3398e38ae4690820d846a5da0e98f49a973f094c
fmaylinch <ferranmaylinch@gmail.com>
2018-07-17 00:02:49 +0200

    Add AccountDTO.units, which is always precise

--- src/main/java/com/codethen/bankapi/api/dto/AccountDTO.java
+++ src/main/java/com/codethen/bankapi/api/dto/AccountDTO.java
@@ -1,21 +1,29 @@
 package com.codethen.bankapi.api.dto;
 
 import com.codethen.bankapi.domain.model.Account;
+import com.codethen.bankapi.domain.model.Amount;
 import com.codethen.bankapi.domain.model.Currency;
 
 public class AccountDTO {
 
     public String username;
+    /** Currency code like "EUR" or "USD" */
     public String currency;
+    /** Available amount in units (e.g. for EUR this will be cents) */
+    public long units;
+    /** Available amount in "whole coins" (e.g. Euros). This value may have precision issues. */
     public double amount;
 
     public static AccountDTO from(Account account) {
 
+        final Amount amount = account.getAmount();
+        final Currency currency = amount.getCurrency();
+
         final AccountDTO accountDTO = new AccountDTO();
         accountDTO.username = account.getUsername();
-        final Currency currency = account.getAmount().getCurrency();
         accountDTO.currency = currency.code;
-        accountDTO.amount = account.getAmount().getUnits() / currency.division;
+        accountDTO.units = amount.getUnits();
+        accountDTO.amount = amount.getUnits() / currency.division;
         return accountDTO;
     }
 }
